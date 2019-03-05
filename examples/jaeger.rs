3ce85dbc4d065f82f2fa348abe341760a5efa029
ccc13 <currantxx@gmail.com>
2019-03-05 11:49:21 +0800

    fix: exmaple

--- examples/jaeger.rs
+++ examples/jaeger.rs
@@ -1,7 +1,3 @@
-extern crate futures;
-extern crate opentracing_rs;
-extern crate tokio;
-
 use std::time::Duration;
 
 use futures::lazy;
@@ -14,9 +10,9 @@ use opentracing_rs::{
 fn main() {
     tokio::run(lazy(move || {
         let mut tracer = JaegerTracer::builder()
-            .probabilistic_sampler(0.50)
+            .const_sampler(true)
             .udp_remote_reporter(
-                "jaeger_example",
+                "rust_jaeger_example",
                 "127.0.0.1:6831".parse().unwrap(),
                 TransportProtocol::ThriftCompact,
                 Duration::from_millis(500),
@@ -26,17 +22,20 @@ fn main() {
             let mut tracer = tracer.clone();
             tokio::spawn(lazy(move || {
                 let span = tracer.span("hello 1").start();
+                println!("hello 1");
 
                 std::thread::sleep(std::time::Duration::from_secs(1));
 
                 let child_span = tracer.span("testing 1").child_of(&span);
                 {
+                    println!("hello 1 -> testing 1");
                     let _child_span = child_span.start();
                     std::thread::sleep(std::time::Duration::from_secs(2));
                 }
 
                 let child_span = tracer.span("testing 2").child_of(&span);
                 {
+                    println!("hello 1 -> testing 2");
                     let _child_span = child_span.start();
                     std::thread::sleep(std::time::Duration::from_secs(2));
                 }
@@ -49,17 +48,20 @@ fn main() {
 
         tokio::spawn(lazy(move || {
             let span = tracer.span("hello 2").start();
+            println!("hello 2");
 
             std::thread::sleep(std::time::Duration::from_secs(1));
 
             let child_span = tracer.span("testing 1").child_of(&span);
             {
+                println!("hello 2 -> testing 2");
                 let _child_span = child_span.start();
                 std::thread::sleep(std::time::Duration::from_secs(2));
             }
 
             let child_span = tracer.span("testing 2").child_of(&span);
             {
+                println!("hello 2 -> testing 2");
                 let _child_span = child_span.start();
                 std::thread::sleep(std::time::Duration::from_secs(2));
             }
